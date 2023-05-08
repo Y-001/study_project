797ee3bac07e66b4725889cef2b9aee3795f0f7e
Y-001 <3276469488@qq.com>
2023-05-08 22:59:03 +0800

    笔记部分改动

--- miniprogram/pageNote/pages/daiban2/daiban2.js
+++ miniprogram/pageNote/pages/daiban2/daiban2.js
@@ -1,100 +1,18 @@
 // pageNote/pages/daiban2/daiban2.js
-const db=wx.cloud.database()
-const _=db.command
 Page({
 
     /**
      * 页面的初始数据
      */
     data: {
-        list:[], //定义一个空数组
-        content:'', //输入的内容
+
     },
-    add(e){
-        //正则验证 不能是空格 内容长度不能大于10 不可以输入特殊符号
-        let valueReg = /^[\u4e00-\u9fa5a-zA-Z\d]{1,10}$/
-        if (!valueReg.test(this.data.content)){
-			wx.showToast({
-				title: '内容不符合规范',
-				icon:"none"
-			})
-			return;
-        }
-        db.collection('todos').add({
-            data:{
-                content:this.data.content,
-                checked:false,
-                push:false
-            }
-        }).then(res=>{
-            wx.showToast({
-              title: '添加成功',
-              icon:'none'
-            })
-            this.setData({
-                content:''
-            })
-            this.getTodoList()
-        })
-    },
-    // 获取代办
-    getTodoList(){
-        db.collection('todos').where({
-            _openid:wx.getStorageSync('openid')
-        }).get().then(res=>{
-            if(res.data.length>0){
-                this.setData({
-                    list:res.data
-                })
-            }
-        })
-    },
-    //单个任务的勾选事件
-    ass(e){
-        let {id}=e.currentTarget.dataset
-        db.collection('todos').doc(id).update({
-            data:{
-                checked:true
-            }
-        }).then(res=>{
-            this.getTodoList()
-        })
-    },
-    // 滑动删除或者别的
-    onClose(event) {
-        let _this=this
-        const { position, instance,name } = event.detail;
-        let {id}=event.currentTarget.dataset
-        switch (position) {
-          case 'left':
-          case 'cell':
-            instance.close();
-            break;
-          case 'right':
-            wx.showModal({
-                title: '确定删除吗？',
-                content: '',
-                success (res) {
-                  if (res.confirm) {
-                      db.collection('todos').doc(id).remove().then(res=>{
-                        _this.getTodoList()
-                      })
-                    instance.close();
-                  } else if (res.cancel) {
-                    // console.log('用户点击取消')
-                    instance.close();
-                  }
-                }
-              })
-            break;
-        }
-      },
 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad(options) {
-        this.getTodoList()
+
     },
 
     /**
